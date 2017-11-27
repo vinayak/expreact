cffaf7ebeece7be019cbb46c160f41a3d43697af
Vinayak Malavade <vinay.malavade@gmail.com>
2017-11-27 20:45:42 +0530

    done with Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Privacy from './assets/SHI-Privacy-Policy-2017.pdf'
+import Terms from './assets/SHI-Social-Media-Terms-of-Use-2017.pdf'
+import {Link} from 'react-router-dom';
 
 class Footer extends Component {
   render() {
     return (
       <footer class="footer">
-        <div class="container">
-          <span class="text-muted">Place sticky footer content here.</span>
+        <div class="container text-center">
+          Copyright &copy; 2017 Sears Holdings India | All Rights Reserved <br/>
+          <Link to={Privacy} target='_blank'>Privacy Policy</Link> | <Link to={Terms} target='_blank'>Social Media Terms of Use</Link>
         </div>
       </footer>
     );
