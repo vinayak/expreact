385daca8666842b6c6f5c29d4b34abd0fb01c454
Vinayak <vinayak.malavade@searshc.com>
2017-11-30 21:04:39 +0530

    update on home

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Here two application are running, React and Express.
 Need to install nodemon globally.
 npm start to run React from root folder.
 nodemon to start Express from server folder.
+
+
+for nodemailer email
+https://medium.com/@manojsinghnegi/sending-an-email-using-nodemailer-gmail-7cfa0712a799
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -25,10 +25,44 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <div style={divStyle} className="outer">
-          <img src={Independence} alt="Inside Sears Holdings India" width="349" height="210" sizes="(max-width: 349px) 100vw, 349px"/>
-          <p className="wp-caption-text">Inside Sears Holdings India</p>
+        <div className="text-center text-uppercase"><h3>About <span className="wlast">sears holdings india</span></h3></div>
+        <div class="row">
+            <div class="col-md-4">
+            <div style={divStyle} className="outer">
+              <img src={Independence} alt="Inside Sears Holdings India" width="349" height="210" sizes="(max-width: 349px) 100vw, 349px"/>
+              <p className="wp-caption-text">Inside Sears Holdings India</p>
+            </div>
+            </div>
+            <div class="col-md-8">
+                <p>Sears IT and Management Services (India) Pvt. Limited, also known as Sears Holdings India (SHI) provides corporate IT services to Sears Holdings Corporation (SHI, NASDAQ:SHLD) and its family of companies including Sears Roebuck and Co. and Kmart Corporation.</p>
+                <p>Established in December 2009, SHI works as an extended arm to the core Information Technology (IT) team of SHC. SHI develops and maintains business critical systems used by SHC and its subsidiaries. Our objective is to assist SHC in making the Sears retail experience simpler and smoother so that members can shop anytime, anywhere.</p>
+                <p>At Sears we offer a wide array of services to advice, transform and optimize business and technology processes while minimizing risk. Our services include systems integration, delivery, machine learning, business analytics, product support, e-commerce, m-commerce and managed services supporting various applications for the Sears retail chain.</p>
+                <p>SHI has its headquarters in Pune, and two more development centers in Bengaluru and Hyderabad. SHI also provides business process management services to SHC.</p>
+            </div>
         </div>
+        <div className="text-center text-uppercase"><h3>sears holdings india <span className="wlast">stands for</span></h3></div>
+        <div class="row">
+            <div class="col-md-4">
+              <div style={divStyle} className="outer">
+                <img src={Innovation} alt="Inside Sears Holdings India" width="260" height="260" sizes="(max-width: 349px) 100vw, 349px"/>
+                <p className="wp-caption-text">Innovation</p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div style={divStyle} className="outer">
+                <img src={Transparency} alt="Inside Sears Holdings India" width="260" height="260" sizes="(max-width: 349px) 100vw, 349px"/>
+                <p className="wp-caption-text">Transparency</p>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div style={divStyle} className="outer">
+                <img src={Empowerment} alt="Inside Sears Holdings India" width="260" height="260" sizes="(max-width: 349px) 100vw, 349px"/>
+                <p className="wp-caption-text">Empowerment</p>
+              </div>
+            </div>
+        </div>
+        <div className="text-center text-uppercase"><h3>About<span className="wlast"> sears holdings Corporation </span></h3></div>
+
       </div>
     /*  <div className="Home">
         <h1>{this.state.message}</h1>
