ccd0632a5e2d6d886d104793fb09439074834d32
Vinayak <vinayak.malavade@searshc.com>
2017-12-02 13:39:27 +0530

    brought in tabs

--- src/pages/MediaCenter/MediaCenter.js
+++ src/pages/MediaCenter/MediaCenter.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import './MediaCenter.css'
 
 class MediaCenter extends Component {
   constructor() {
     super();
     this.state = { message: {}};
+  }
+  componentDidMount() {
+    const jsCode = `
+      $(function() {
+        $('.tabs nav a').on('click', function() {
+          show_content($(this).index());
+        });
+
+        show_content(0);
+
+        function show_content(index) {
+          // Make the content visible
+          $('.tabs .content.visible').removeClass('visible');
+          $('.tabs .content:nth-of-type(' + (index + 1) + ')').addClass('visible');
+
+          // Set the tab to selected
+          $('.tabs nav a.selected').removeClass('selected');
+          $('.tabs nav a:nth-of-type(' + (index + 1) + ')').addClass('selected');
+        }
+      });
+    `;
+    new Function(jsCode)();
   }
   // componentDidMount() {
   //   return fetch('/api/blog')
@@ -11,7 +34,31 @@ class MediaCenter extends Component {
   //     .then(json => this.setState({ message: json }));
   // }
   render() {
-    return <h1>Media Center</h1>
+    return (
+      <div>
+        <h1>Media Center</h1>
+        <div className="tabs">
+          <nav>
+            <a>In the News</a>
+            <a>Latest Events</a>
+            <a>Social Causes</a>
+            <a>Press Releases</a>
+          </nav>
+          <div className="content">
+            <p>Content #1</p>
+          </div>
+          <div className="content">
+            <p>Content #2</p>
+          </div>
+          <div className="content">
+            <p>Content #3</p>
+          </div>
+          <div className="content">
+            <p>Content #4</p>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
